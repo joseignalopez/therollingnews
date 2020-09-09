be31f04df7d32615b2d77e6c795867142fcc6790
Rosario <roserranoar@gmail.com>
2020-09-09 20:54:41 -0300

    se agrego pagina inicio.js

--- src/components/principal/Inicio.js
+++ src/components/principal/Inicio.js
@@ -1,11 +1,46 @@
-import React from 'react';
-
+import React from "react";
+import Card from 'react-bootstrap/Card';
 const Inicio = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="container"> 
+    <section className="row">
+      <div className=" col-md-6  my-3">
+      <Card>
+        <Card.Img variant="top" src="img1.jpg" />
+        <Card.Body>
+          <Card.Text className=""> 
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      </div>
+      <div className=" col-md-3 my-3 ">
+      <Card>
+        <Card.Img variant="top" src="img1.jpg" />
+        <Card.Body>
+          <Card.Text> 
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      </div>
+      <div className="col-md-3 my-3">
+      <Card>
+        <Card.Img variant="top" src="img1.jpg"/>
+        <Card.Body>
+          <Card.Text> 
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+
+      </div>
+      </section>
+    </div>
+  );
 };
 
 export default Inicio;
