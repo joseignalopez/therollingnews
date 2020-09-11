d345981fe7ca9c55a917eba9de5ae35d8d059219
Luciano <lucianochipolina@gmail.com>
2020-09-11 11:59:17 -0300

    footer maquetado

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,13 +1,49 @@
-const { default: src } = require("*.bmp");
-
-import React from 'react';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebookSquare} from "@fortawesome/free-brands-svg-icons";
+import { faInstagramSquare} from "@fortawesome/free-brands-svg-icons";
+import { faTwitterSquare} from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope} from "@fortawesome/free-solid-svg-icons";
 
 const Footer = () => {
-    return (
-        <div>
-            
+  return (
+    <div>
+      <section className="rojo py-4">
+        <div className="text-center row text-light">
+          <div className="col-sm-12 col-md-4 text-center list-unstyled mb-4">
+              <li className="pb-1"><a href="#" className="text-light">Acerca de nosotros</a></li>
+              <li className="py-1"><a href="#" className="text-light">Términos y condiciones</a></li>
+              <li className="py-1"><a href="#" className="text-light">Cookies</a></li>
+              <li className="py-1"><a href="#" className="text-light">Contacto</a></li>
+              <li className="py-1"><a href="#" className="text-light">Publicitar con nosotros</a></li>
+              <li className="pt-1"><a href="#" className="text-light">Políticas de privacidad</a></li>
+          </div>
+          <div className="col-sm-12 col-md-4 text-center sm-mb-4">
+            <img src="IMG-20200909-WA0004.jpg" className="w-75" alt="The Rolling Times"/>
+            <ul className="list-unstyled d-flex justify-content-center p-0 mt-4">
+                <li><a href="https://facebook.com" target="_blank" className="text-light mr-2"><FontAwesomeIcon icon={faFacebookSquare} size="3x"></FontAwesomeIcon></a></li>
+                <li><a href="https://instagram.com" target="_blank" className="text-light mx-2"><FontAwesomeIcon icon={faInstagramSquare} size="3x"></FontAwesomeIcon></a></li>
+                <li><a href="https://twitter.com" target="_blank" className="text-light mx-2"><FontAwesomeIcon icon={faTwitterSquare} size="3x"></FontAwesomeIcon></a></li>
+                <li><a href="https://gmail.com" target="_blank" className="text-light ml-2"><FontAwesomeIcon icon={faEnvelope} size="3x"></FontAwesomeIcon></a></li>
+            </ul>
+          </div>
+          <div className="col-sm-12 col-md-4 text-center list-unstyled"> 
+                <li className="pb-1"><a href="#" className="text-light"><b>Secciones:</b></a></li>
+                <li className="py-1"><a href="#" className="text-light">Actualidad</a></li>
+                <li className="py-1"><a href="#" className="text-light">Espectáculos</a></li>
+                <li className="py-1"><a href="#" className="text-light">Tecnología</a></li>
+                <li className="py-1"><a href="#" className="text-light">Deportes</a></li>
+                <li className="pt-1"><a href="#" className="text-light">Política</a></li>
+                <li className="pt-1"><a href="#" className="text-light">Economía</a></li>
+                <li className="pt-1"><a href="#" className="text-light">Salud</a></li>      
+          </div>
         </div>
-    );
+      </section>
+      <footer className="text-center py-2 bg-dark text-light">
+        &copy; Todos los derechos reservados - The Rolling Times
+      </footer>
+    </div>
+  );
 };
 
 export default Footer;
--- src/components/principal/Inicio.js
+++ src/components/principal/Inicio.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 const Inicio = () => {
     return (
         <div>
