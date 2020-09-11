cd7d7914cc485b34322449d29b4ff79389f071fb
Luciano <lucianochipolina@gmail.com>
2020-09-11 12:14:31 -0300

    se perfeccionaron detalles del footer

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -7,10 +7,10 @@ import { faEnvelope} from "@fortawesome/free-solid-svg-icons";
 
 const Footer = () => {
   return (
-    <div>
-      <section className="rojo py-4">
+    <div className="rojo">
+      <section className="container py-4">
         <div className="text-center row text-light">
-          <div className="col-sm-12 col-md-4 text-center list-unstyled mb-4">
+          <div className="col-sm-12 col-md-4 list-unstyled mb-4 d-flex flex-column justify-content-around">
               <li className="pb-1"><a href="#" className="text-light">Acerca de nosotros</a></li>
               <li className="py-1"><a href="#" className="text-light">Términos y condiciones</a></li>
               <li className="py-1"><a href="#" className="text-light">Cookies</a></li>
@@ -18,8 +18,8 @@ const Footer = () => {
               <li className="py-1"><a href="#" className="text-light">Publicitar con nosotros</a></li>
               <li className="pt-1"><a href="#" className="text-light">Políticas de privacidad</a></li>
           </div>
-          <div className="col-sm-12 col-md-4 text-center sm-mb-4">
-            <img src="IMG-20200909-WA0004.jpg" className="w-75" alt="The Rolling Times"/>
+          <div className="col-sm-12 col-md-4 sm-mb-4">
+            <img src="IMG-20200909-WA0004.jpg" width="70%" alt="The Rolling Times"/>
             <ul className="list-unstyled d-flex justify-content-center p-0 mt-4">
                 <li><a href="https://facebook.com" target="_blank" className="text-light mr-2"><FontAwesomeIcon icon={faFacebookSquare} size="3x"></FontAwesomeIcon></a></li>
                 <li><a href="https://instagram.com" target="_blank" className="text-light mx-2"><FontAwesomeIcon icon={faInstagramSquare} size="3x"></FontAwesomeIcon></a></li>
@@ -27,7 +27,7 @@ const Footer = () => {
                 <li><a href="https://gmail.com" target="_blank" className="text-light ml-2"><FontAwesomeIcon icon={faEnvelope} size="3x"></FontAwesomeIcon></a></li>
             </ul>
           </div>
-          <div className="col-sm-12 col-md-4 text-center list-unstyled"> 
+          <div className="col-sm-12 col-md-4 list-unstyled d-flex flex-column justify-content-around"> 
                 <li className="pb-1"><a href="#" className="text-light"><b>Secciones:</b></a></li>
                 <li className="py-1"><a href="#" className="text-light">Actualidad</a></li>
                 <li className="py-1"><a href="#" className="text-light">Espectáculos</a></li>
