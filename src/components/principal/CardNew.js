d99b9c90add0afc89201a54cf299d2689cbeb69c
Nicolas Bertini Argañaras <nicolas_bertini@hotmail.com>
2020-09-11 21:12:12 -0300

    Cards de noticias por categoria finalizado. Falta aplicar css

--- src/components/principal/CardNew.js
+++ src/components/principal/CardNew.js
@@ -1,19 +1,16 @@
 import React from "react";
-import { Card, Button } from "react-bootstrap";
+import { Card} from "react-bootstrap";
 
 const CardNew = (props) => {
-    console.log("En cardNew")
-    console.log(props)
+  console.log("En cardNew");
+  console.log(props);
   return (
     <div>
       <Card style={{ width: "18rem" }}>
         <Card.Img variant="top" src={props.noticia.Imagen} />
         <Card.Body>
-  <Card.Title>{props.noticia.Titulo}</Card.Title>
-          <Card.Text>
-            {props.noticia.DetalleCorto}
-          </Card.Text>
-          
+          <Card.Title>{props.noticia.Titulo}</Card.Title>
+          <Card.Text>{props.noticia.DetalleCorto}</Card.Text>
         </Card.Body>
       </Card>
     </div>
--- src/components/principal/CategoryNew.js
+++ src/components/principal/CategoryNew.js
@@ -1,13 +1,16 @@
 import React, { Fragment } from "react";
 import CardNew from "./CardNew";
+import CardDeck from 'react-bootstrap/CardDeck'
 
 const CategoryNew = (props) => {
   return (
     <Fragment>
       <h2>{props.nombreCategoria}</h2>
-      {
-        props.noticiasDeCategoria.map(noticia=><CardNew key={noticia.id} noticia={noticia}></CardNew>)
-      }
+      <CardDeck className="mb-3">
+        {props.noticiasDeCategoria.map((noticia) => (
+          <CardNew key={noticia.id} noticia={noticia}></CardNew>
+        ))}
+      </CardDeck>
     </Fragment>
   );
 };
