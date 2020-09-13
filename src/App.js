854421c489974e018bdd871b5ef43db479743273
Carlos M. Rodriguez <carlosnotengo1@gmail.com>
2020-09-13 16:31:23 -0300

    se agrego la fecha y la hora y se uso el sistema de grillas para poder ubicarlas en el mismo renglon

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'; 
-import Header from './components/common/Header'
-import Footer from './components/common/Footer'
-import Inicio from './components/principal/Inicio';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/common/Header";
+import Footer from "./components/common/Footer";
+import Inicio from "./components/principal/Inicio";
+import Tiempo from "./components/Apiclima/Tiempo";
+import Reloj from "./components/Apiclima/Reloj";
 
 function App() {
   return (
-    
     <Router>
       <Header></Header>
+      <section className="container">
+        <div className="row text-center d-flex justify-content-center">
+          <Tiempo className="col-sm-12 col-md-9 col-lg-9"></Tiempo>
+          <Reloj className="col-sm-12 col-md-3 col-lg-3"></Reloj>
+        </div>
+      </section>
+
       <Switch>
         <Route exact path="/">
           <Inicio></Inicio>
