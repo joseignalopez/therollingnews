c7221c0a3dc31a0bd2706924dd701f10a8ad4035
Rosario <roserranoar@gmail.com>
2020-09-14 16:20:31 -0300

    se agrego estilo al  icono del  boton buscar

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,7 +1,7 @@
 import React from "react";
  import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faFacebook} from "@fortawesome/free-brands-svg-icons";
-import {faInstagram} from "@fortawesome/free-brands-svg-icons";
+import {faFacebook,faInstagram} from "@fortawesome/free-brands-svg-icons";
+
 
 const Footer = () => {
   return (
