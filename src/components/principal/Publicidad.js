17a1f350632b78fcf9d66706cb0a8b0041a32239
Rosario <roserranoar@gmail.com>
2020-09-18 15:22:40 -0300

    se arreglo subnav y tambien se cambio imagenes de componente publicidad

--- src/components/principal/Publicidad.js
+++ src/components/principal/Publicidad.js
@@ -1,31 +1,33 @@
 import React from "react";
 import "./style/publicidad.css";
 
+
+
 const Publicidad = () => {
   return (
-    <div >
-      <div className=" d-flex justify-content-center mt-3  ">
-        <div className="align-item"><img src="videopu.gif" width="150px" height="150px"/></div>
+    <div  className="">
+      <div className=" d-flex justify-content-center mt-3 mt-3  ">
+        <div className="align-item"><img src={process.env.PUBLIC_URL+"/videopu.gif"} alt="publicidad" width="150px" height="150px"/></div>
        <hr/>
         
       </div>
       <div className=" d-flex justify-content-center mt-3  ">
-        <div className="align-item"><img src="tenor.gif" width="150px" height="250px"/></div>
+        <div className="align-item"><img src={process.env.PUBLIC_URL+"/tenor.gif"} alt="publicidad" width="150px" height="250px"/></div>
        <hr/>
         
       </div>
       <div className=" d-flex justify-content-center mt-3  ">
-        <div className="align-item"><img src="boa.gif" width="150px" height="150px"/></div>
+        <div className="align-item"><img src={process.env.PUBLIC_URL+"/boa.gif"} alt="publicidad" width="150px" height="150px"/></div>
        <hr/>
         
       </div>
       <div className=" d-flex justify-content-center mt-3  ">
-        <div className="align-item"><img src="images.jpg" width="150px" height="150px"/></div>
+        <div className="align-item"><img src={process.env.PUBLIC_URL+"/images.jpg"} alt="publicidad" width="150px" height="150px"/></div>
        <hr/>
         
       </div>
       <div className=" d-flex justify-content-center mt-3  ">
-        <div className="align-item"><img src="coto.jpg" width="150px" height="150px"/></div>
+        <div className="align-item"><img src={process.env.PUBLIC_URL+"/coto.jpg"} alt="publicidad" width="150px" height="150px"/></div>
        <hr/>
         
       </div>
--- src/components/principal/SeccionesHeader.js
+++ src/components/principal/SeccionesHeader.js
@@ -4,50 +4,51 @@ import React from "react";
 const SeccionesHeader = () => {
   return (
 
-      <div className="subnav-content justify-content-end">
-        <ul className="d-flex  flex-column flex-lg-row  ">
-          <li className="nav-item">
-              <a className="nav-link " href="#">
+      <div className="subnav-content d-flex justify-content-center">
+        <ul className="d-flex  flex-column flex-lg-row">
+        
+          <li className="nav-item my-1  ">
+              <a className="nav-link " >
             Actualidad
                </a>
-          </li>
-          <li className="nav-item">
-              <a className=" nav-link" href="#">
+          </li> 
+          <li className="nav-item my-1">
+              <a className=" nav-link" >
             Espectáculos
                </a>
           </li>
-          <li className="nav-item">
-              <a className="nav-link" href="#">
+          <li className="nav-item my-1 ">
+              <a className="nav-link" >
             Tecnología
                </a>
           </li>
-          <li className="nav-item">
-              <a className=" nav-link" href="#">
+          <li className="nav-item my-1">
+              <a className=" nav-link">
             Deportes
                </a>
           </li>
-          <li className="nav-item">
-              <a className=" nav-link" href="#">
+          <li className="nav-item my-1">
+              <a className=" nav-link" >
             Política
                </a>
           </li>
-          <li className="nav-item">
-              <a className="nav-link " href="#">
+          <li className="nav-item my-1">
+              <a className="nav-link " >
             Economía
                </a>
           </li>
-          <li className="nav-item">
-              <a className="nav-link " href="#">
+          <li className="nav-item my-1">
+              <a className="nav-link " >
                          Salud
                </a>
           </li>
-          <li className="nav-item ">
-              <a className="nav-link " href="#">
+          <li className="nav-item my-1 ">
+              <a className="nav-link " >
                        Fotografía
                </a>
           </li>
-          <li className="nav-item">
-              <a className="nav-link " href="#">
+          <li className="nav-item my-1">
+              <a className="nav-link " >
             Covid-19
                </a>
           </li>
