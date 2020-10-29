d50fec34ece680e018678ed47fdc8ea73d237e3d
Rosario <roserranoar@gmail.com>
2020-10-29 18:31:31 -0300

    se cambio redireccionamiento de usurios con etiquetas link

--- src/components/principal/SeccionesHeader.js
+++ src/components/principal/SeccionesHeader.js
@@ -5,13 +5,14 @@ import { Link } from "react-router-dom";
 
 
 
+
 const SeccionesHeader = (props) => {
   return (
-    <div className="subnav-content d-flex justify-content-center text-white" >
-      <ListGroup horizontal className="d-flex flex-lg-row justify-content-between">
+    <div className="subnav-content d-flex justify-content-center " >
+      <ListGroup  className="d-flex flex-lg-row justify-content-between">
       {props.categorias.map((categoria) => (
-          <ListGroup.Item key={categoria._id} className="nav-item bg-transparent active">
-            <Link onClick={() => props.setSeccionVisible(!props.seccionVisible)} to={`/Categoria/${categoria.nombre}`} className="nav-link">
+          <ListGroup.Item key={categoria._id} className="nav-item bg-transparent">
+            <Link onClick={() => props.setSeccionVisible(!props.seccionVisible)} to={`/Categoria/${categoria.nombre}`} className="nav-link" >
               {categoria.nombre}
             </Link>
           </ListGroup.Item>
